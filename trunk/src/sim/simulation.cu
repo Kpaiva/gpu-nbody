530e17c7c6e7cc88048e18de4e31bb67de6832d7
bouz013@gmail.com <bouz013@gmail.com@f8d74518-6b4f-fa2f-1fce-df15697f6096>
2013-03-07 16:18:47 +0000

    - near there (gotta do render)

--- trunk/src/sim/simulation.cu
+++ trunk/src/sim/simulation.cu
@@ -4,152 +4,174 @@
 #include "simbody.cu"
 #include "..\timer.h"
 
-typedef struct {
-	SimBody* array;
-	unsigned size;
+typedef struct
+{
+    SimBody *array;
+    unsigned size;
 } BodyArray;
 
-BodyArray MakeArray(thrust::device_vector<SimBody>& arr) {
-	BodyArray ba = { thrust::raw_pointer_cast(&arr[0]), arr.size() };
-	return ba;
+BodyArray MakeArray(thrust::device_vector<SimBody> &arr)
+{
+    BodyArray ba = { thrust::raw_pointer_cast(&arr[0]), arr.size() };
+    return ba;
 }
 
-void __global__ SimCalc(BodyArray a) {
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	if(idx < a.size) {
-		a.array[idx].ResetForce();
+void __global__ SimCalc(BodyArray a)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < a.size)
+    {
+        a.array[idx].ResetForce();
 
-		for(size_t j = 0; j < a.size; ++j) {
-			if(idx != j) { 
-				a.array[idx].AddForce(a.array[j]);
-			}
-		}
-	}
+        for (size_t j = 0; j < a.size; ++j)
+        {
+            if (idx != j)
+            {
+                a.array[idx].AddForce(a.array[j]);
+            }
+        }
+    }
 }
 
-void __global__ SimTick(BodyArray a, float dt) {
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	if(idx < a.size) {
-		a.array[idx].Tick(dt);	
-	}
+void __global__ SimTick(BodyArray a, float dt)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < a.size)
+    {
+        a.array[idx].Tick(dt);
+    }
 }
 
 Simulation::Simulation(void) : sampleCount_(-1), numBlocks_(0), numThreads_(0) { }
 
-Simulation& Simulation::GetInstance(void) {
-	static Simulation self;
-	return self;
+Simulation &Simulation::GetInstance(void)
+{
+    static Simulation self;
+    return self;
 }
 
-int Simulation::Setup(int argc, char* argv[]) {
-	if(argc < 2) {
-		std::cout << "Invalid number of arguments." << std::endl <<
-			"Usage: " << argv[0] << " [num bodies] <max samples>" << std::endl;
-		return 1;
-	}
-	if(argc == 3) {
-		int do_samples = atoi(argv[2]);
-		if(do_samples < 1 || do_samples > 10240) {
-			std::cout << "** Invalid number of samples, must be between 1 and 10240. **" << std::endl;
-			return 1;
-		}
-		sampleCount_ = do_samples;
-	}
-	int num_bodies = atoi(argv[1]);
-	if(num_bodies < 0 || num_bodies > 16384) {
-		std::cout << "** Invalid number of bodies, must be between 1 and 16384. **" << std::endl;
-		return 1;
-	}
-	std::cout << "Setting up " << num_bodies << " bodies." << std::endl;
-	srand(time(NULL));
-	bodies_.reserve(num_bodies);
-	for(unsigned i = 0; i < num_bodies; ++i)
-		bodies_.push_back(SimBody(
-		random(1.0E11f,3.0E11f),
-		random(-6.0E11f,9.0E11f),
-		random(-1000.0f,1000.0f),
-		random(-1000.0f,1000.0f),
-		random(1.0E9f, 1.0E31f)));
-	std::cout << "Configuring CUDA... " << std::endl;
+int Simulation::Setup(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "Invalid number of arguments." << std::endl <<
+                  "Usage: " << argv[0] << " [num bodies] <max samples>" << std::endl;
+        return 1;
+    }
+    if (argc == 3)
+    {
+        int do_samples = atoi(argv[2]);
+        if (do_samples < 1 || do_samples > 10240)
+        {
+            std::cout << "** Invalid number of samples, must be between 1 and 10240. **" << std::endl;
+            return 1;
+        }
+        sampleCount_ = do_samples;
+    }
+    int num_bodies = atoi(argv[1]);
+    if (num_bodies < 0 || num_bodies > 16384)
+    {
+        std::cout << "** Invalid number of bodies, must be between 1 and 16384. **" << std::endl;
+        return 1;
+    }
+    std::cout << "Setting up " << num_bodies << " bodies." << std::endl;
+    srand(time(NULL));
+    bodies_.reserve(num_bodies);
+    for (unsigned i = 0; i < num_bodies; ++i)
+        bodies_.push_back(SimBody(
+                              random(1.0E11f, 3.0E11f),
+                              random(-6.0E11f, 9.0E11f),
+                              random(-1000.0f, 1000.0f),
+                              random(-1000.0f, 1000.0f),
+                              random(1.0E9f, 1.0E31f)));
+    std::cout << "Configuring CUDA... " << std::endl;
 
-	int device;
-	cudaDeviceProp prop;
-	cudaError_t err = cudaGetLastError();
-	if(err != cudaSuccess) {
-		std::cout << cudaGetErrorString(err) << std::endl;
-		return 1;
-	}
-	err = cudaGetDevice(&device);
-	if(err != cudaSuccess) {
-		std::cout << "Error getting CUDA device... aborting." << std::endl;
-		return 1;
-	}
-	err = cudaGetDeviceProperties(&prop, device);
-	if(err == cudaErrorInvalidDevice) {
-		std::cout << "Invalid CUDA device found... aborting." << std::endl;
-		return 1;
-	}
-	int max_threads = prop.maxThreadsDim[0];
+    int device;
+    cudaDeviceProp prop;
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        std::cout << cudaGetErrorString(err) << std::endl;
+        return 1;
+    }
+    err = cudaGetDevice(&device);
+    if (err != cudaSuccess)
+    {
+        std::cout << "Error getting CUDA device... aborting." << std::endl;
+        return 1;
+    }
+    err = cudaGetDeviceProperties(&prop, device);
+    if (err == cudaErrorInvalidDevice)
+    {
+        std::cout << "Invalid CUDA device found... aborting." << std::endl;
+        return 1;
+    }
+    int max_threads = prop.maxThreadsDim[0];
 
-	numBlocks_ = (bodies_.size() + max_threads - 1) / max_threads;
-	numThreads_ = max_threads;
+    numBlocks_ = (bodies_.size() + max_threads - 1) / max_threads;
+    numThreads_ = max_threads;
 
-	std::cout << "CUDA setup complete. Using:" << std::endl <<
-		"\tBlocks: " << numBlocks_ << std::endl <<
-		"\tThreads: " << numThreads_ << std::endl;
-	std::cout << "Completed setup... computing... " << std::endl;
-	return 0;
+    std::cout << "CUDA setup complete. Using:" << std::endl <<
+              "\tBlocks: " << numBlocks_ << std::endl <<
+              "\tThreads: " << numThreads_ << std::endl;
+    std::cout << "Completed setup... computing... " << std::endl;
+    return 0;
 }
 
-int Simulation::Run(void) {
-	running_ = true;
-	float timeStep = 25000.0f;
+int Simulation::Run(void)
+{
+    running_ = true;
+    float timeStep = 25000.0f;
 
-	unsigned sample = 0;
+    unsigned sample = 0;
 
-	Timer timer;
-	timer.start();
-	if(sampleCount_ > 0)
-		std::cout << "Running test for " << sampleCount_ << " samples..." << std::endl;
+    Timer timer;
+    timer.start();
+    if (sampleCount_ > 0)
+        std::cout << "Running test for " << sampleCount_ << " samples..." << std::endl;
 
-	BodyArray arr = MakeArray(bodies_);		
-	while(running_) {
-		SimCalc<<<numBlocks_, numThreads_>>>(arr);
-		//Ensure that we have done all calculations before we move on to tick.
-		cudaThreadSynchronize();
+    BodyArray arr = MakeArray(bodies_);
+    while (running_)
+    {
+        SimCalc <<< numBlocks_, numThreads_>>>(arr);
+        //Ensure that we have done all calculations before we move on to tick.
+        cudaThreadSynchronize();
 
-		SimTick<<<numBlocks_, numThreads_>>>(arr, timeStep);
-		//Ensure that we have ticked all before we move to calculate the average.
-		cudaThreadSynchronize();
+        SimTick <<< numBlocks_, numThreads_>>>(arr, timeStep);
+        //Ensure that we have ticked all before we move to calculate the average.
+        cudaThreadSynchronize();
 
-		++sample;	
+        ++sample;
 
-		if(sampleCount_ < 0) {
-			if(sample % SAMPLE_RATE == 0) {
-				std::cout << sample << " Samples taken avg. " << std::fixed 
-					<< float(timer.getElapsedTimeInMilliSec() / (float)sample) 
-					<< " ms/sample. " << float((float)sample / timer.getElapsedTimeInSec())
-					<< " samples/sec. " << std::endl;
-			}
-			if(timer.getElapsedTimeInSec() > TIME_TO_LIVE)
-				running_ = false;
-		}
-		else if(sample == sampleCount_) {
-			running_ = false;	
-		}
-	}
-	timer.stop();
-	std::cout << sample << " Samples taken avg. " << std::fixed 
-		<< float(timer.getElapsedTimeInMilliSec() / (float)sample) 
-		<< " ms/sample. " << float((float)sample / timer.getElapsedTimeInSec())
-		<< " samples/sec. " << std::endl;
-	if(sampleCount_ > 0)
-		std::cout << "Total elapsed time: " << timer.getElapsedTimeInSec() << " seconds." << std::endl;
-	std::cout << "Completed the test with " << sample << " samples. Press any key to exit." << std::endl;
+        if (sampleCount_ < 0)
+        {
+            if (sample % SAMPLE_RATE == 0)
+            {
+                std::cout << sample << " Samples taken avg. " << std::fixed
+                          << float(timer.getElapsedTimeInMilliSec() / (float)sample)
+                          << " ms/sample. " << float((float)sample / timer.getElapsedTimeInSec())
+                          << " samples/sec. " << std::endl;
+            }
+            if (timer.getElapsedTimeInSec() > TIME_TO_LIVE)
+                running_ = false;
+        }
+        else if (sample == sampleCount_)
+        {
+            running_ = false;
+        }
+    }
+    timer.stop();
+    std::cout << sample << " Samples taken avg. " << std::fixed
+              << float(timer.getElapsedTimeInMilliSec() / (float)sample)
+              << " ms/sample. " << float((float)sample / timer.getElapsedTimeInSec())
+              << " samples/sec. " << std::endl;
+    if (sampleCount_ > 0)
+        std::cout << "Total elapsed time: " << timer.getElapsedTimeInSec() << " seconds." << std::endl;
+    std::cout << "Completed the test with " << sample << " samples. Press any key to exit." << std::endl;
 #if IS_LINUX
-	std::cin >> sample;
+    std::cin >> sample;
 #else
-	system("pause");
+    system("pause");
 #endif
-	return 0;
+    return 0;
 }
--- trunk/src/win/body.cu
+++ trunk/src/win/body.cu
@@ -1,71 +1,32 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "common.h"
-#include <xmmintrin.h>
 #include <cassert>
-#include <cuda_runtime.h>
+#include <../sim/simbody.cu>
 
-class Body {
-	sf::Sprite	sprite_;
-	double		mass_;
-
-	inline double ComputeGC(double m1, double m2, double d);	
+template <typename T>
+class _Body : _SimBody<T>
+{
+    sf::Sprite  sprite_;
 public:
-	sf::Vector2<double> Force;
-	sf::Vector2<double> Velocity;
-	sf::Vector2<double> Position;
+    Body(sf::Image *image, double rx = 0.0, double ry = 0.0, double vx = 0.0, double vy = 0.0, double mass = 0.0)
+        : sprite_(*image), _SimBody(rx, ry, vx, vy, mass)
+    {
+        sprite_.SetCenter(image->GetWidth() / 2.0f, image->GetHeight() / 2.0f);
+    }
 
-	Body(sf::Image* image, double rx = 0.0, double ry = 0.0, double vx = 0.0, double vy = 0.0, double mass = 0.0)
-		: sprite_(*image) {	
-		sprite_.SetCenter(image->GetWidth()/2.0f, image->GetHeight()/2.0f);
-		mass_ = mass;
-		Velocity.x = vx;
-		Velocity.y = vy;
-		Force.x = 0.0;
-		Force.y = 0.0;
-		Position.x = rx;
-		Position.y = ry;
-	}
+    sf::Sprite &Sprite() const
+    {
+        return (sf::Sprite &)sprite_;
+    }
 
-	CUDA_CALLABLE_MEMBER void Tick(double dt) {
-		Velocity.x += dt * (Force.x / mass_);
-		Velocity.y += dt * (Force.y / mass_);
-		Position.x += dt * Velocity.x;
-		Position.y += dt * Velocity.y;
-	}	
-	
-	CUDA_CALLABLE_MEMBER void AddForce(const Body& b) {
-		double dx = b.Position.x - Position.x;
-		double dy = b.Position.y - Position.y;
-		double r = sqrt(dx*dx + dy*dy);
-		double F = ComputeGC(mass_, b.GetMass(), r);
-		Force.x += F * (dx / r);
-		Force.y += F * (dy / r);
-	}
-	
-	CUDA_CALLABLE_MEMBER void ResetForce() {
-		Force.x = Force.y = 0;
-	}
+    operator sf::Sprite &() const
+    {
+        return (sf::Sprite &)sprite_;
+    }
 
-	sf::Sprite& Sprite() const {
-		return (sf::Sprite&)sprite_;
-	}
-
-	operator sf::Sprite&() const {
-		return (sf::Sprite&)sprite_;
-	}
-
-	CUDA_CALLABLE_MEMBER void SetSpritePosition( float x, float y ) {
-		sprite_.SetPosition(x, y);
-	}
-
-	double SetMass(double mass) {
-		return mass_ = mass;
-	}
-
-	double GetMass() const {
-		return mass_;
-	}
-
-	//no destructor
+    CUDA_CALLABLE_MEMBER void SetSpritePosition( float x, float y )
+    {
+        sprite_.SetPosition(x, y);
+    }
 };
