ba67e30db0a7df7cab7378443f13e42178f30036
Clinton.Bale@gmail.com <Clinton.Bale@gmail.com@f8d74518-6b4f-fa2f-1fce-df15697f6096>
2013-03-07 23:38:17 +0000

    Removed USE_SSE again.

--- trunk/src/common.h
+++ trunk/src/common.h
@@ -4,11 +4,7 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-
-//#define USE_SSE
-
 #define IS_SIMULATION 0
-
 //Amount of samples before average is output on screen.
 #define SAMPLE_RATE 1000
 //Time to run the simulation in seconds.
