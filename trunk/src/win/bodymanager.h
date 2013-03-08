cb83374a25ac66ba7209d55e67290f887dd514c5
bouz013@gmail.com <bouz013@gmail.com@f8d74518-6b4f-fa2f-1fce-df15697f6096>
2013-03-08 00:04:55 +0000

    separated files

--- trunk/src/win/bodymanager.h
+++ trunk/src/win/bodymanager.h
@@ -2,37 +2,49 @@
 //02/06/2013
 
 #pragma once
+#ifndef _BODYMANAGER_H_
+#define _BODYMANAGER_H_
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <vector>
-
+#include "imagemanager.h"
+#include "body.cu"
+#include "../sim/simbody.cu"
+#include <thrust/device_vector.h>
+#include <cuda_runtime.h>
 #include "imagemanager.h"
 
-class Body;
+class _Body;
 
 class BodyManager : sf::NonCopyable {
 	BodyManager(void);
 	~BodyManager(void);
 
-	std::vector<Body> bodies_;
+	thrust::device_vector<SimBody> bodies_;
+	std::vector<_Body> sprites_;
 	ImageManager& imageManager_;
 	sf::RenderWindow* app_;
-	double solarRadius_;	
+	float solarRadius_;	
 	size_t zoomLevel_;
+
+	unsigned numBlocks_;
+    unsigned numThreads_;
+    BodyArray arr_;
 public:
 	static BodyManager& GetInstance();
 		
-	void Tick(double gameTime);
+	void Tick(float gameTime);
 	void Render(void);
-	bool Init(int count, double radius, sf::RenderWindow* app);
+	bool Init(int count, float radius, sf::RenderWindow* app);
 	bool InitFromFile(const char* fileName, sf::RenderWindow* app);
-	void AddBody(const Body& body);
-	void ClearBodies();
-	double GetSolarRadius() const;
-
+	void AddBody(const SimBody &body);
+	void AddBodySprite(const _Body &body);
+	float GetSolarRadius() const;
 	void IncreaseZoom();
 	void DecreaseZoom();
 	int ZoomLevel() const;
+	unsigned getBlocks() const;
+	unsigned getThreads() const;
 };
-
+#endif
